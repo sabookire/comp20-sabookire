a590567ec92c90828e5560a3e45e97ca40acb06d
sabookire@gmail.com <Sylvie@Sylvie-Abookires-MacBook-Pro.local>
2015-03-08 14:05:53 -0400

    adding readme

---
+++ messages/README.txt
@@ -0,0 +1,10 @@
+Lab 5: Messages
+Read Me
+
+Sylvie Abookire
+
+index.html and lab.js display messages from data.json.
+
+All aspects are correctly implemented to the best of my knowledge.
+
+3 hours spent completing assignment, mostly working with git and debugging JavaScript console errors.
--- messages/lab.js
+++ messages/lab.js
@@ -1,23 +1,23 @@
-function.parse() {
+function parse() {
 	// Step 1: create instance of object
 	request = new XMLHttpRequest();
 
-	// Step 2: create/"open" HTTP request
-	request.open("GET", "data.json", true);
-
 	// Step 3: set up a way to manage response --to a function
 	request.onreadystatechange = parseData;
+
+	// Step 2: create/"open" HTTP request
+	request.open("GET", "data.json", true);
 
 	// Step 4: execute response
 	request.send();
 }
 
-function.parseData() {
+function parseData() {
 	if (request.readyState == 4 && request.status == 200) {
 		messagesDiv = document.getElementById("messages");
 		converted = JSON.parse(request.responseText);
 		for (i = 0; i< converted.length; i++) {
-			messagesDiv.innerHTML += "<p>" + converted[i]["content"] + " - " + converted[i]['username'] + "</p>";
+			messagesDiv.innerHTML += "<p>" + converted[i]['content'] + " - " converted[i]['username'] + "</p>";
 		}
 	}
 	else if (request.readyState == 4 && request.status != 200) {
